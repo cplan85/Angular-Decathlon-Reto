f432bfa988f7448af645a176ec2a0f6f451075b5
cplan85 <carlos_planchart@live.com>
2022-07-25 23:30:31 +0200

    began my reviews component

--- src/app/components/my-reviews/my-reviews.component.ts
+++ src/app/components/my-reviews/my-reviews.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-reviews',
   templateUrl: './my-reviews.component.html',
-  styleUrls: ['./my-reviews.component.scss']
+  styleUrls: ['./my-reviews.component.scss'],
 })
 export class MyReviewsComponent implements OnInit {
+  myTopReviews = [
+    {
+      image: '../assets/img/elements/hiking group.jpg',
+      hearts: 5000,
+      points: 25,
+      originalReviewURL: '/productReview/{productId}/1',
+      created: '06/22',
+      tags: ['outdoors', 'hike & camp', 'exercise', 'extreme weather'],
+    },
+    {
+      image: '../assets/img/elements/beach1.webp',
+      hearts: 2700,
+      points: 25,
+      originalReviewURL: '/productReview/{productId}/1',
+      created: '06/22',
+      tags: ['summer', 'beach', 'exercise', 'extreme weather'],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
