1a313b101772a0f6c20cf83fd7191f820e7cf8d5
Carlos Planchart <carlos_planchart@live.com>
2022-07-26 15:37:48 +0200

    updated landing component

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -6,16 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
+
+  parsedArray: any[] = [];
   
   productsMock = 
-  [{
+  `[{
     "_id": {
       "$oid": "62dfc2810c1410270048577d"
     },
     "productId": 0,
     "brand": "Decathlon",
     "category": "Zapatillas",
-    "description": "\"Zapatillas para amateur",
+    "description": "Zapatillas para amateur",
     "image": "https://contents.mediadecathlon.com/p2155651/k$81fc073b39ce6c1402394f10a36c303b/sq/zapatillas-running-hombre-asics-gel-windhawk-azul-blanco.jpg?format=auto&f=650x650",
     "price": 98.9,
     "title": "Zapatillas runner D1000"
@@ -26,11 +28,11 @@ export class LandingComponent implements OnInit {
     "productId": 1,
     "brand": "Decathlon",
     "category": "Zapatillas",
-    "description": "\"Zapatillas para profesional",
-    "image": "https://www.decathlon.es/es/p/zapatillas-running-asics-gel-glyde-3-hombre-negro-amarillo/_/R-p-339930?mc=8757275",
+    "description": "Zapatillas para profesional",
+    "image": "https://contents.mediadecathlon.com/p2163732/k$6bbf369f34069ad7695a759107dc80e9/sq/zapatillas-running-asics-gel-glyde-3-hombre-negro-amarillo.jpg?f=960x960",
     "price": 158.49,
     "title": "Zapatillas runner D2000"
-  }]
+  }]`
 
   userData = [{
     "_id": {
@@ -198,7 +200,17 @@ export class LandingComponent implements OnInit {
     },
   ];
 
+  getJSON() {
+    const parsedProducts: any[] = JSON.parse(this.productsMock);
+
+    parsedProducts.forEach(item => {
+      this.parsedArray.push(item)
+    })
+  }
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getJSON()
+  }
 }
